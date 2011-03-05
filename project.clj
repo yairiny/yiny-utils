326d8eaa8aa3c5bc953012f3cb2ea5366b0b29d1
Yair Iny <yair.iny@gmail.com>
2011-03-05 21:29:46 +1100

    updated deps for lein-clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject org.clojars.yiny/utils "0.1.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "My utilities"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [lein-clojars "0.6.0"]])
 
