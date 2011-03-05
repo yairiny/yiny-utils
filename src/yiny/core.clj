08008e75d1cc21cc603198e17d312a9bd67d5101
Yair Iny <yair.iny@gmail.com>
2011-03-05 21:15:23 +1100

    reorging tests

--- src/yiny/core.clj
+++
@@ -1,1 +0,0 @@
-(ns yiny-utils.core)
--- test/yiny_utils/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns yiny-utils.test.core
-  (:use [yiny-utils.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
